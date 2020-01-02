6b39587f29f63d61e71dee48b014f3580a3ef9a3
asdf <tasos.kapetzoglou@gmail.com>
2020-01-02 18:16:31 +0100

    fixed linting errors

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -2,7 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    console.log(this.modelFor('bands.band'));
     return this.modelFor('bands.band');
   }
 });
