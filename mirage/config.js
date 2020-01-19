33e832efcd77a1c128965c3c639b40ab2d3b2839
asdf <tasos.kapetzoglou@gmail.com>
2020-01-19 14:02:11 +0100

    TDD/ search (qp) and sorting !!

--- mirage/config.js
+++ mirage/config.js
@@ -1,8 +1,10 @@
-export default function() {
+export default function () {
 
   this.get('/bands');
   this.get('/bands/:id');
+  this.get('bands/:id/songs', function (schema, request) {
+    let id = request.params.id;
+    return schema.songs.where({ bandId: id }) });
   this.post('/bands');
-  this.get('bands/:id/songs');
   this.post('/songs');
 }
